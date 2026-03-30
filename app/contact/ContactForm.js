c3e8e89cbb61637fa960f436dcfce00fc7080282
vardhan-kamerkar <vardhan.kamerkar@gmail.com>
2026-03-30 14:11:44 +0530

    update

--- app/contact/ContactForm.js
+++ app/contact/ContactForm.js
@@ -1,8 +1,9 @@
 "use client";
 import { useState } from "react"; 
 import { useSearchParams } from "next/navigation";
-export default function ContactForm(){
     const params = useSearchParams();
+    
+    export default function ContactForm() { 
      const [form, setForm] = useState({ 
         name: "", 
         email: "", 
@@ -10,10 +11,30 @@ export default function ContactForm(){
         product: params.get("product") || "", 
         message: "", 
     });
+
+const submit = async () => { 
+    await fetch("/api/enquiry", { 
+        method: "POST", 
+        headers:{
+            "Content-Type":"application/json",
+        },
+        body: JSON.stringify(form), 
+    }); 
+    alert("Submitted"); 
+};
     return(
         <div>
-            <h1>contact page</h1>
-            <p>Product: {product}</p>
+            <div className="min-h-screen flex justify-center items-center bg-gray-100">
+    <div className="bg-white p-6 rounded-xl shadow w-full max-w-md"> 
+    <h1 className="text-xl font-bond mb-4">Contact</h1> 
+    <input className="w-full border p-2 mb-2" placeholder="Name" onChange={e => setForm({ ...form, name: e.target.value })} /> 
+    <input className="w-full border p-2 mb-2" placeholder="Email" onChange={e => setForm({ ...form, email: e.target.value })} /> 
+    <input className="w-full border p-2 mb-2" placeholder="Phone" onChange={e => setForm({ ...form, phone: e.target.value })} /> 
+    <input className="w-full border p-2 mb-2 bg-gray-200"value={form.product} onChange={e => setForm({ ...form,product: e.target.value})} /> 
+    <textarea className="w-full border p-2 mb-2" onChange={e => setForm({ ...form, message: e.target.value })} /> 
+        <button onClick={submit}className="w-full bg-blue-500 text-white py-2 rounded">Submit</button> 
+        </div> 
         </div>
-    )
+        </div>
+    );
 }
